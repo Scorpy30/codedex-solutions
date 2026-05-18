76177aad77abdd6ace9dfba2f802c2ac46156a9a
Tarush <tarush3008@gmail.com>
2026-05-18 13:48:00 +0530

    Python Ch-3: Control Flow solutions

---
+++ python/03-control-flow/12_grades.py
@@ -0,0 +1,9 @@
+# Grades 💯
+#Codédex solutions
+
+grade = 60
+
+if grade > 55:
+  print('You passed')
+else:
+  print('You failed')
---
+++ python/03-control-flow/16_sorting_hat.py
@@ -0,0 +1,62 @@
+# Sorting Hat 🎩
+# Codédex solutions
+
+points_g = 0
+points_r = 0
+points_h = 0
+points_s = 0
+
+print("Q1) Do you like Dawn or Dusk?")
+print("1) Dawn")
+print("2) Dusk")
+
+option = int(input())
+if option == 1:
+  points_g = points_g + 1
+  points_r = points_r + 1
+elif option == 2:
+  points_h = points_h + 1
+  points_s = points_s + 1
+else:
+  print('Wrong input')
+
+print("Q2) When I’m dead, I want people to remember me as:")
+print("1) The Good")
+print("2) The Great")
+print("3) The Wise")
+print("4) The Bold")
+
+option = int(input())
+if option == 1:
+  points_h = points_h + 2
+elif option == 2:
+  points_s = points_s + 2
+elif option == 3:
+  points_r = points_r + 2
+elif option == 4:
+  points_g = points_g + 2
+else:
+  print('Wrong input')
+
+print("Q3)  Which kind of instrument most pleases your ear?")
+print("1) The violin")
+print("2) The trumpet")
+print("3) The piano")
+print("4) The drum")
+
+option = int(input())
+if option == 1:
+  points_s = points_s + 4
+elif option == 2:
+  points_h = points_h + 4
+elif option == 3:
+  points_r = points_r + 4
+elif option == 4:
+  points_g = points_g + 4
+else:
+  print('Wrong input')
+
+print(f'Gryffindor: {points_g}')
+print(f'Hufflepuff: {points_h}')
+print(f'Ravenclaw: {points_r}')
+print(f'Slytherin: {points_s}')
